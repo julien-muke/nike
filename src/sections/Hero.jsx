6ced7f538301614ee321c9dd9b28022c44814205
julien-muke <julien.muke@gmail.com>
2023-08-20 19:36:46 +0200

    add animation

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -13,7 +13,7 @@ const Hero = () => {
       id='home'
       className='w-full flex xl:flex-row flex-col justify-center min-h-screen gap-10 max-container'
     >
-      <div className='relative xl:w-2/5 flex flex-col justify-center items-start w-full  max-xl:padding-x pt-28'>
+      <div className='relative xl:w-2/5 flex flex-col justify-center items-start w-full  max-xl:padding-x pt-28 animate-in slide-in-from-left delay-300 duration-3000'>
         <p className='text-xl font-montserrat text-coral-red'>
           Our Summer collections
         </p>
